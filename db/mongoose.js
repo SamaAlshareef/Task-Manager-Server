fe48e192824c1fb0b669d1c8eff37e1eb2a8d7b7
SamaAlshareef <sama.elsherif@student.guc.edu.eg>
2020-08-25 14:14:28 +0200

    Connecting to Atlas MongoDB

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/TaskManager',{useNewUrlParser:true}).then(()=>{
+mongoose.connect('mongodb+srv://mongoDatabases:2020_mongo@cluster0.rdajt.mongodb.net/TaskManager?retryWrites=true&w=majority',{useNewUrlParser:true}).then(()=>{
     console.log('Connecting to mongo Database successfully');
 }).catch((e)=>{
     console.log('Error while connecting to the database');
